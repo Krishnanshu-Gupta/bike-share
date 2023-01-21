919d12ff494b65a1c04943544f3d3a39f5cf2d0b
krishgupta-x <krishnanshu@gmail.com>
2023-01-21 14:53:18 -0800

    v1

---
+++ src/StartPage.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './StartPage.css';
+
+function StartPage() {
+  return (
+    <div className="App">
+          <img src="bike.jpeg" className='image-container' alt="logo" />
+    </div>
+  );
+}
+
+export default StartPage;
